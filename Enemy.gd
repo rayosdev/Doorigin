403511f884be070f5891b22cf1baa46554e37412
Krocgar <nilserlingh@gmail.com>
2023-02-04 19:12:09 +0100

    pathfinding functioning towards target and back

--- Enemy.gd
+++ Enemy.gd
@@ -1,28 +1,22 @@
 extends KinematicBody2D
 
-signal target_reached
-
 export(float) var MAX_SPEED = 105.0
-
 onready var navigation_agent = $NavigationAgent2D
-
 var velocity = Vector2.ZERO
 var last_move_velocity = Vector2.ZERO
 var move_direction = Vector2.ZERO
-var did_arrive = false
 var original_position
 var player
 onready var pursue_timer = $Timer
-
+var wait_at_home = true
+var waiting = false
 
 func _ready():
-	# ensure villagers are initially set to their origin
 	original_position = position
 	player = get_parent().find_node("Player")
-	set_target_location(player.global_position)
+	set_target_location(original_position)
 
 func set_target_location(target:Vector2) -> void:
-	did_arrive = false
 	navigation_agent.set_target_location(target)
 
 func _physics_process(_delta):
@@ -32,21 +26,24 @@ func _physics_process(_delta):
 	
 	if not _arrived_at_location():
 		velocity = move_and_slide(velocity)
-	elif not did_arrive:
-		did_arrive = true
-		emit_signal("target_reached")
-		
+	elif !wait_at_home :
+		set_target_location(player.position)
+	elif !waiting:
+		waiting = true
+		pursue_timer.wait_time = 2
+		pursue_timer.start()
+
 func _arrived_at_location() -> bool:
 	return navigation_agent.is_navigation_finished()
 
 func on_root_hurt():
-	navigation_agent.set_target_location(original_position)
-	pursue_timer.start()
+	set_target_location(original_position)
+	wait_at_home = true
 
-
-func _on_Area2D_area_entered(area):
+func _on_Area2D_area_entered(_area):
 	on_root_hurt()
 
-
 func _on_Timer_timeout():
-	navigation_agent.set_target_location(player.position)
+	set_target_location(player.position)
+	wait_at_home = false
+	waiting = false
